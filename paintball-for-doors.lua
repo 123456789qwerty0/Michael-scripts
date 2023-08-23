395b2d8573d6bf08280f376414a2db314b9af5fb
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2023-08-23 16:21:00 -0400

    Add files via upload

---
+++ paintball-for-doors.lua
@@ -0,0 +1,4 @@
+_G.Hold = true -- Set to false to click to paint / Set to true to shoot paint balls
+_G.ClassicColours = false -- Set to false to get customized coloured paint balls / Set to true to get the classic coloured paint balls
+_G.Size = Vector3.new(0.5, 0.5, 0.5) -- The higher the bigger, The lower the smaller
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iimateiYT/Scripts/main/Paintball%20Gun.lua"))()
